3e907a35359eeee1eb6a341ba1ff4be276d79e04
MundusUni <unus.mundi@gmail.com>
2022-05-30 18:10:35 -0300

    Currículo v0.1

--- js/demo.js
+++ js/demo.js
@@ -8,70 +8,70 @@ jQuery( document ).ready(function( $ ) {
    * set settings panel button
    */
   function setSettingsBtn() {
-    var settingsDiv = '<a data-toggle="tooltip" data-placement="right" class="settings-btn addon-left" title="Setting Me Up" href="javascript:;"><i class="fa fa-cog"></i></a> \
+    var settingsDiv = '<a data-toggle="tooltip" data-placement="right" class="settings-btn addon-left" title="Configurações" href="javascript:;"><i class="fa fa-cog"></i></a> \
       <!-- .settings-btn --> \
       <div class="settings-panel panel visible-lg-block"> \
-        <div class="panel-heading panel-default"><p class="panel-title">Header Style</p></div> \
+        <div class="panel-heading panel-default"><p class="panel-title">Estilo do Header</p></div> \
         <div class="panel-body header-style"> \
-          <a data-header-style="classic" class="header-item">Classic</a> \
-          <a data-header-style="bimg-fullw" class="header-item">BG Image FullW</a> \
-          <a data-header-style="bimg" class="header-item">BG Image</a> \
-          <a data-header-style="bname" class="header-item">Big Name</a> \
-          <a data-header-style="min" class="header-item">MIN</a> \
-          <a data-header-style="norm" class="header-item">Just Name</a> \
-          <a data-header-style="binfo" class="header-item">INFO</a> \
+          <a data-header-style="classic" class="header-item">Clássico</a> \
+          <a data-header-style="bimg-fullw" class="header-item">Imagem Full BG</a> \
+          <a data-header-style="bimg" class="header-item">Imagem Background</a> \
+          <a data-header-style="bname" class="header-item">Nome Gigante</a> \
+          <a data-header-style="min" class="header-item">Perfil Minimo</a> \
+          <a data-header-style="norm" class="header-item">Somente o Nome</a> \
+          <a data-header-style="binfo" class="header-item">Informações</a> \
         </div> \
         <!-- .header-style --> \
-        <div class="panel-heading panel-default"><p class="panel-title">Theme Color</p></div> \
+        <div class="panel-heading panel-default"><p class="panel-title">Cor do Tema</p></div> \
         <div class="panel-body theme-color"> \
-          <a data-theme-color="default" class="default theme-item" title="Default - Gray">&nbsp;</a> \
-          <a data-theme-color="red" class="red theme-item" title="Red">&nbsp;</a> \
-          <a data-theme-color="blue" class="blue theme-item" title="Blue">&nbsp;</a> \
-          <a data-theme-color="green" class="green theme-item" title="Green">&nbsp;</a> \
-          <a data-theme-color="brown" class="brown theme-item" title="Brown">&nbsp;</a> \
-          <a data-theme-color="blue-gray" class="blue-gray theme-item" title="Blue Gray">&nbsp;</a> \
+          <a data-theme-color="default" class="default theme-item" title="Default - Cinza">&nbsp;</a> \
+          <a data-theme-color="red" class="red theme-item" title="Vermelho">&nbsp;</a> \
+          <a data-theme-color="blue" class="blue theme-item" title="Azul">&nbsp;</a> \
+          <a data-theme-color="green" class="green theme-item" title="Verde">&nbsp;</a> \
+          <a data-theme-color="brown" class="brown theme-item" title="Marrom">&nbsp;</a> \
+          <a data-theme-color="blue-gray" class="blue-gray theme-item" title="Cinza Azulado">&nbsp;</a> \
         </div> \
         <!-- .theme-color --> \
-        <div class="panel-heading panel-default"><p class="panel-title">BG Color</p></div> \
+        <div class="panel-heading panel-default"><p class="panel-title">Cor do BG</p></div> \
         <div class="panel-body bg-color"> \
-          <a data-bg-color="default-bg" class="default-bg theme-item" title="Gray">&nbsp;</a> \
-          <a data-bg-color="red-bg" class="red-bg theme-item" title="Red">&nbsp;</a> \
-          <a data-bg-color="deep-purple-bg" class="deep-purple-bg theme-item" title="Deep Purple">&nbsp;</a> \
-          <a data-bg-color="blue-bg" class="blue-bg theme-item" title="Blue">&nbsp;</a> \
-          <a data-bg-color="green-bg" class="green-bg theme-item" title="Green">&nbsp;</a> \
-          <a data-bg-color="brown-bg" class="brown-bg theme-item" title="Brown">&nbsp;</a> \
-          <a data-bg-color="blue-gray-bg" class="blue-gray-bg theme-item" title="Blue Gray">&nbsp;</a> \
-          <a data-bg-color="gray-lighter-higher-bg" class="gray-lighter-higher-bg theme-item" title="Gray Lighter Higher">&nbsp;</a> \
-          <a data-bg-color="gray-lighter-high-bg" class="gray-lighter-high-bg theme-item" title="Gray Lighter High">&nbsp;</a> \
-          <a data-bg-color="gray-lighter-bg" class="gray-lighter-bg theme-item" title="Gray Lighter">&nbsp;</a> \
+          <a data-bg-color="default-bg" class="default-bg theme-item" title="Cinza">&nbsp;</a> \
+          <a data-bg-color="red-bg" class="red-bg theme-item" title="Vermelho">&nbsp;</a> \
+          <a data-bg-color="deep-purple-bg" class="deep-purple-bg theme-item" title="Púrpura Profundo">&nbsp;</a> \
+          <a data-bg-color="blue-bg" class="blue-bg theme-item" title="Azul">&nbsp;</a> \
+          <a data-bg-color="green-bg" class="green-bg theme-item" title="Verde">&nbsp;</a> \
+          <a data-bg-color="brown-bg" class="brown-bg theme-item" title="Marrom">&nbsp;</a> \
+          <a data-bg-color="blue-gray-bg" class="blue-gray-bg theme-item" title="Cinza Azulado">&nbsp;</a> \
+          <a data-bg-color="gray-lighter-higher-bg" class="gray-lighter-higher-bg theme-item" title="Cinza Claro">&nbsp;</a> \
+          <a data-bg-color="gray-lighter-high-bg" class="gray-lighter-high-bg theme-item" title="Cinza Médio">&nbsp;</a> \
+          <a data-bg-color="gray-lighter-bg" class="gray-lighter-bg theme-item" title="Cinza Forte">&nbsp;</a> \
         </div> \
         <!-- .bg-color --> \
-        <div class="panel-heading panel-default"><p class="panel-title">BG Pattern</p></div> \
+        <div class="panel-heading panel-default"><p class="panel-title">Padrão do BG</p></div> \
         <div class="panel-body bg-ptn"> \
-          <a data-bg-ptn="svgeneration-blue-jean" class="svgeneration-blue-jean theme-item" title="Blue Jean">&nbsp;</a> \
-          <a data-bg-ptn="svgeneration-construction-paper" class="svgeneration-construction-paper theme-item" title="Construction Paper">&nbsp;</a> \
-          <a data-bg-ptn="svgeneration-cross-stripes" class="svgeneration-cross-stripes theme-item" title="Cross Stripes">&nbsp;</a> \
-          <a data-bg-ptn="svgeneration-graph-paper" class="svgeneration-graph-paper theme-item" title="Graph Paper">&nbsp;</a> \
-          <a data-bg-ptn="svgeneration-oriental-roof" class="svgeneration-oriental-roof theme-item" title="Oriental Roof">&nbsp;</a> \
-          <a data-bg-ptn="svgeneration-patchwork" class="svgeneration-patchwork theme-item" title="Patchwork">&nbsp;</a> \
-          <a data-bg-ptn="svgeneration-zags" class="svgeneration-zags theme-item" title="Zags">&nbsp;</a> \
-          <a data-bg-ptn="trianglify-blue" class="trianglify-blue theme-item" title="Blue">&nbsp;</a> \
-          <a data-bg-ptn="trianglify-green-light" class="trianglify-green-light theme-item" title="Green-light">&nbsp;</a> \
-          <a data-bg-ptn="trianglify-gray" class="trianglify-gray theme-item" title="Gray">&nbsp;</a> \
-          <a data-bg-ptn="trianglify-ocean-blue" class="trianglify-ocean-blue theme-item" title="Ocean Blue">&nbsp;</a> \
-          <a data-bg-ptn="trianglify-ocean-green" class="trianglify-ocean-green theme-item" title="Ocean Green">&nbsp;</a> \
-          <a data-bg-ptn="trianglify-orange" class="trianglify-orange theme-item" title="Orange">&nbsp;</a> \
-          <a data-bg-ptn="trianglify-pink" class="trianglify-pink theme-item" title="Pink">&nbsp;</a> \
-          <a data-bg-ptn="trianglify-purple" class="trianglify-purple theme-item" title="Purple">&nbsp;</a> \
-          <a data-bg-ptn="trianglify-red" class="trianglify-red theme-item" title="Red">&nbsp;</a> \
+          <a data-bg-ptn="svgeneration-blue-jean" class="svgeneration-blue-jean theme-item" title="Jeans Azul">&nbsp;</a> \
+          <a data-bg-ptn="svgeneration-construction-paper" class="svgeneration-construction-paper theme-item" title="Papel de Construção">&nbsp;</a> \
+          <a data-bg-ptn="svgeneration-cross-stripes" class="svgeneration-cross-stripes theme-item" title="Faixas Cruzadas">&nbsp;</a> \
+          <a data-bg-ptn="svgeneration-graph-paper" class="svgeneration-graph-paper theme-item" title="Papel Gráfico">&nbsp;</a> \
+          <a data-bg-ptn="svgeneration-oriental-roof" class="svgeneration-oriental-roof theme-item" title="Telhado Oriental">&nbsp;</a> \
+          <a data-bg-ptn="svgeneration-patchwork" class="svgeneration-patchwork theme-item" title="Colcha de Retalhos">&nbsp;</a> \
+          <a data-bg-ptn="svgeneration-zags" class="svgeneration-zags theme-item" title="ZigZags">&nbsp;</a> \
+          <a data-bg-ptn="trianglify-blue" class="trianglify-blue theme-item" title="Azul">&nbsp;</a> \
+          <a data-bg-ptn="trianglify-green-light" class="trianglify-green-light theme-item" title="Verde Claro">&nbsp;</a> \
+          <a data-bg-ptn="trianglify-gray" class="trianglify-gray theme-item" title="Cinza">&nbsp;</a> \
+          <a data-bg-ptn="trianglify-ocean-blue" class="trianglify-ocean-blue theme-item" title="Azul Oceano">&nbsp;</a> \
+          <a data-bg-ptn="trianglify-ocean-green" class="trianglify-ocean-green theme-item" title="Verde Oceano">&nbsp;</a> \
+          <a data-bg-ptn="trianglify-orange" class="trianglify-orange theme-item" title="Laranja">&nbsp;</a> \
+          <a data-bg-ptn="trianglify-pink" class="trianglify-pink theme-item" title="Rosa">&nbsp;</a> \
+          <a data-bg-ptn="trianglify-purple" class="trianglify-purple theme-item" title="Púrpura">&nbsp;</a> \
+          <a data-bg-ptn="trianglify-red" class="trianglify-red theme-item" title="Vermelho">&nbsp;</a> \
         </div> \
         <!-- .bg-ptn --> \
-        <div class="panel-heading panel-default"><p class="panel-title">BG Image</p></div> \
+        <div class="panel-heading panel-default"><p class="panel-title">Imagem de BG</p></div> \
         <div class="panel-body bg-img"> \
-          <a data-bg-img="gratisography-28h" class="gratisography-28h theme-item" title="Sea">&nbsp;</a> \
-          <a data-bg-img="gratisography-56h" class="gratisography-56h theme-item" title="Rail">&nbsp;</a> \
-          <a data-bg-img="gratisography-134h" class="gratisography-134h theme-item" title="Toy">&nbsp;</a> \
-          <a data-bg-img="gratisography-161h" class="gratisography-161h theme-item" title="Smile cook">&nbsp;</a> \
+          <a data-bg-img="gratisography-28h" class="gratisography-28h theme-item" title="Azul">&nbsp;</a> \
+          <a data-bg-img="gratisography-56h" class="gratisography-56h theme-item" title="Rosa">&nbsp;</a> \
+          <a data-bg-img="gratisography-134h" class="gratisography-134h theme-item" title="Azul Claro">&nbsp;</a> \
+          <a data-bg-img="gratisography-161h" class="gratisography-161h theme-item" title="Azul Não-Tão-Claro">&nbsp;</a> \
         </div> \
         <!-- .bg-img --> \
       </div> \
